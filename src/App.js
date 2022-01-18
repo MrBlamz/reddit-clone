2e05eb05fee8120e84d314e1e247b1ff6ebc4db2
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-01-18 10:09:02 +0000

    Create PostCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Box, Container } from '@chakra-ui/react';
+import { postExample } from './data';
 import Navbar from './components/Navbar';
+import PostCard from './components/PostCard';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Container maxW='full' padding={{ base: 2, md: '20px 24px' }}>
+        <Box>
+          <PostCard
+            isFirst
+            communityName='Stoicism'
+            comments={postExample.comments}
+            title={postExample.title}
+            content={postExample.content}
+            author={postExample.author}
+            upVotes={postExample.upVotes}
+            downVotes={postExample.downVotes}
+            timestamp={postExample.timestamp}
+          />
+        </Box>
+      </Container>
     </div>
   );
 }
---
+++ src/components/PostCard/index.js
@@ -0,0 +1,245 @@
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+  useMediaQuery,
+  VStack,
+} from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { BiDownvote, BiShare, BiUpvote } from 'react-icons/bi';
+import { BsArrowsAngleExpand, BsBookmark, BsCardText } from 'react-icons/bs';
+import { VscComment } from 'react-icons/vsc';
+
+const ActionButton = ({ ariaLabel, icon, text, ...rest }) => (
+  <Button
+    variant='action'
+    aria-label={ariaLabel}
+    leftIcon={icon}
+    fontSize='xl'
+    {...rest}
+  >
+    <Text fontSize='xs'>{text}</Text>
+  </Button>
+);
+
+const VotingButtons = ({ upVotes, downVotes, ...rest }) => (
+  <Stack
+    alignItems='center'
+    justifyContent='center'
+    lineHeight={0}
+    direction={{ base: 'row', lg: 'column' }}
+    {...rest}
+  >
+    <IconButton
+      variant='action'
+      aria-label='Upvote'
+      icon={<BiUpvote />}
+      minW={0}
+      p='0px 5px'
+      fontSize='xl'
+      color={useColorModeValue('brand.iconLight', 'brand.iconDark')}
+      _hover={{
+        color: 'brand.primary',
+        bg: useColorModeValue('brand.iconFadedLight', 'brand.iconFadedDark'),
+      }}
+    />
+    <Text fontWeight='bold'>{upVotes - downVotes}</Text>
+    <IconButton
+      variant='action'
+      aria-label='Downvote'
+      icon={<BiDownvote />}
+      minW={0}
+      p='0px 5px'
+      fontSize='lg'
+      color={useColorModeValue('brand.iconLight', 'brand.iconDark')}
+      _hover={{
+        color: 'brand.secondary',
+        bg: useColorModeValue('brand.iconFadedLight', 'brand.iconFadedDark'),
+      }}
+    />
+  </Stack>
+);
+
+const ExpandButton = ({ ...rest }) => (
+  <IconButton
+    variant='action'
+    aria-label='Expand post'
+    icon={<BsArrowsAngleExpand />}
+    transform='rotate(90deg)'
+    fontSize='xl'
+    color={useColorModeValue('brand.iconLight', 'brand.iconDark')}
+    {...rest}
+  />
+);
+
+const DocumentIcon = ({ ...rest }) => (
+  <Center
+    ml={2}
+    bg={useColorModeValue('brand.iconBgLight', 'brand.iconBgDark')}
+    w='130px'
+    h='full'
+    {...rest}
+  >
+    <Icon
+      as={BsCardText}
+      boxSize={5}
+      color={useColorModeValue('brand.iconLight', 'brand.iconDark')}
+    />
+  </Center>
+);
+
+const Header = ({ title, communityName, author, timestamp }) => (
+  <Flex direction='column' alignItems='flex-start'>
+    <Box>
+      <Text as='h3' fontWeight='600'>
+        {title}
+      </Text>
+    </Box>
+    <HStack fontSize='xs'>
+      <Link as={RouterLink} to={`/r/${communityName}`}>
+        <Text>{`r/${communityName}`}</Text>
+      </Link>
+      <Text>{`Posted by u/${author}`}</Text>
+      <Text>{timestamp}</Text>
+    </HStack>
+  </Flex>
+);
+
+const PostCard = ({
+  author,
+  comments,
+  communityName,
+  content,
+  downVotes,
+  isFirst,
+  isLast,
+  timestamp,
+  title,
+  upVotes,
+}) => {
+  const ACTION_BUTTONS = [
+    {
+      ariaLabel: 'Open Comments',
+      icon: <VscComment />,
+      text: `${comments.length} ${
+        comments.length === 1 ? 'Comment' : 'Comments'
+      }`,
+    },
+    {
+      ariaLabel: 'Share post',
+      icon: <BiShare style={{ transform: 'rotateY(180deg)' }} />,
+      text: 'Share',
+    },
+    {
+      ariaLabel: 'Save post',
+      icon: <BsBookmark />,
+      text: 'Save',
+    },
+  ];
+  const [isMobile] = useMediaQuery('(max-width: 62rem)');
+  const borderColor = useColorModeValue(
+    'brand.borderLight',
+    'brand.borderDark'
+  );
+
+  return (
+    <Flex
+      width='full'
+      minH='90px'
+      position='relative'
+      bg={useColorModeValue('brand.light', 'brand.dark')}
+      px={{ base: 2, lg: 0 }}
+      pt={{ base: 2, lg: 0 }}
+      border='1px'
+      borderTopRadius={isFirst ? '5px' : 0}
+      borderBottomRadius={isLast ? '5px' : 0}
+      borderColor={borderColor}
+      borderBottomColor={isLast ? borderColor : 'transparent'}
+      _hover={{
+        cursor: 'pointer',
+        border: '1px',
+        borderColor: useColorModeValue('brand.dark', 'brand.light'),
+      }}
+    >
+      {isMobile ? (
+        <>
+          <VStack width='full' alignItems='flex-start'>
+            <Header
+              title={title}
+              author={author}
+              communityName={communityName}
+              timestamp={timestamp}
+            />
+
+            <Flex>
+              <VotingButtons upVotes={upVotes} downVotes={downVotes} />
+              <Flex ml={2} flexWrap='wrap'>
+                {ACTION_BUTTONS.map((btn) => (
+                  <ActionButton
+                    p={1}
+                    key={btn.ariaLabel}
+                    ariaLabel={btn.ariaLabel}
+                    icon={btn.icon}
+                    text={btn.text}
+                  />
+                ))}
+              </Flex>
+            </Flex>
+          </VStack>
+          <Box
+            display={{ base: 'none', md: 'block' }}
+            position='relative'
+            pb={2}
+          >
+            <DocumentIcon />
+            <ExpandButton position='absolute' bottom='2' right='0' />
+          </Box>
+        </>
+      ) : (
+        <>
+          <Flex p={2}>
+            <VotingButtons upVotes={upVotes} downVotes={downVotes} />
+            <DocumentIcon />
+          </Flex>
+          <Box>
+            <Flex p={2} pb={0} pl={0} w='full' h='full'>
+              <Flex direction='column' px={2}>
+                <Header
+                  title={title}
+                  author={author}
+                  communityName={communityName}
+                  timestamp={timestamp}
+                />
+                <HStack mt='auto'>
+                  <ExpandButton />
+                  <Divider orientation='vertical' h='50%' />
+                  <Flex>
+                    {ACTION_BUTTONS.map((btn) => (
+                      <ActionButton
+                        key={btn.text}
+                        ariaLabel={btn.ariaLabel}
+                        icon={btn.icon}
+                        text={btn.text}
+                      />
+                    ))}
+                  </Flex>
+                </HStack>
+              </Flex>
+            </Flex>
+          </Box>
+        </>
+      )}
+    </Flex>
+  );
+};
+
+export default PostCard;
