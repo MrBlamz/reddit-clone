ddd8e79e1dfa0e3b86f4796401d0efca2aa8501c
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-01-18 10:06:01 +0000

    Remove blue highlight color on tap

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -4,6 +4,7 @@ const styles = {
   global: (props) => ({
     body: {
       bg: mode('brand.bgLight', 'brand.bgDark')(props),
+      WebkitTapHighlightColor: 'rgba(255, 255, 255, 0)',
     },
   }),
 };
