fa5bcccb25ef5de7a4cd2eef10d2ce490ecc3b27
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-01-25 18:21:51 +0000

    Move Navbar to App component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import { Box, Container } from '@chakra-ui/react';
-import Navbar from '../components/Navbar';
 import PostCard from '../components/PostCard';
 import { postExample } from '../data';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
       <Container maxW='full' padding={{ base: 2, md: '20px 24px' }}>
         <Box>
           <PostCard
