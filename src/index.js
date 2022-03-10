27922976603affba59b8626b606c74c751ee6c01
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-03-10 18:13:28 +0000

    Setup redux store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/provider';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider resetCSS theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
