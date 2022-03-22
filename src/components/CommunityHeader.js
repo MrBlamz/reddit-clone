67edca65700e3889ddf3fed29a09dde785a59fa8
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-03-22 12:01:55 +0000

    Create CommunityHeader component

---
+++ src/components/CommunityHeader.js
@@ -0,0 +1,26 @@
+import { Box, Heading } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+const CommunityHeader = ({ communityName }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate(`/r/${communityName}`);
+
+  return (
+    <Box
+      as='header'
+      py={{ base: 5, md: 7 }}
+      bg='green.400'
+      onClick={handleClick}
+      _hover={{ cursor: 'pointer' }}
+    >
+      <Heading
+        mx={{ base: 3, md: 72 }}
+        fontSize={{ base: 20, md: 22 }}
+        color='white'
+      >{`r/${communityName}`}</Heading>
+    </Box>
+  );
+};
+
+export default CommunityHeader;
