86fd828d2445a475508adff394c1de636f54ecd1
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-03-22 21:11:27 +0000

    Create hook to listen for auth status changes

---
+++ src/hooks/useUser.js
@@ -0,0 +1,43 @@
+import { useDispatch } from 'react-redux';
+import { actions as authActions } from '../store/auth';
+import { auth } from '../utils/firebase/auth';
+import { fetchUserData } from '../utils/firebase/firestore';
+
+const useUser = () => {
+  const dispatch = useDispatch();
+
+  const fetchUserFromLocalStorage = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const userData = JSON.parse(localStorage.getItem('userData'));
+
+    if (user) {
+      dispatch(authActions.fetchUserFromLocalStorage({ user, userData }));
+    }
+  };
+
+  const listenForAuthChanges = () => {
+    return auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userData = await fetchUserData(user.uid);
+
+        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('userData', JSON.stringify(userData));
+
+        dispatch(
+          authActions.login({
+            user: user.toJSON(),
+            userData,
+          })
+        );
+      } else {
+        localStorage.removeItem('user');
+        localStorage.removeItem('userData');
+        dispatch(authActions.logout());
+      }
+    });
+  };
+
+  return { fetchUserFromLocalStorage, listenForAuthChanges };
+};
+
+export default useUser;
