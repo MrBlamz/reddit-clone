a52220c91701f5c59647c71142b3f3d1b11d1d5a
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-03-27 16:35:48 +0100

    Create DropdownMenu component

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,58 @@
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { IoMdArrowDropdown } from 'react-icons/io';
+
+const DropdownMenu = ({ description, onClick, options = [] }) => {
+  const color = useColorModeValue('brand.secondary', 'brand.primary');
+  const [selected, setSelected] = useState(options[0]);
+
+  const isSelected = (item) => item === selected;
+
+  const handleClick = (event) => {
+    setSelected(event.target.textContent);
+    onClick(event);
+  };
+
+  return (
+    <Menu isLazy gutter={-5} autoSelect={false}>
+      <MenuButton
+        w='fit-content'
+        as={Button}
+        rightIcon={<IoMdArrowDropdown />}
+        variant='transparent'
+        fontSize='14px'
+        padding={0}
+        _hover={{ outline: 'none' }}
+        color={color}
+      >
+        {description}
+      </MenuButton>
+      <MenuList
+        minW={0}
+        bg={useColorModeValue('brand.light', 'brand.dark')}
+        fontSize='14px'
+        onClick={handleClick}
+      >
+        {Array.isArray(options) &&
+          options.map((option) => (
+            <MenuItem
+              key={option}
+              fontWeight={isSelected(option) && 'bold'}
+              color={isSelected(option) && color}
+            >
+              {option}
+            </MenuItem>
+          ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default DropdownMenu;
