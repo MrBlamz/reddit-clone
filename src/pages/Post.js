0ec6631ab171e3ffc6a79b3d19eaf4614b0531cf
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-04-04 18:59:15 +0100

    Create Post page

---
+++ src/pages/Post.js
@@ -0,0 +1,149 @@
+import { Box, Divider, Flex, Stack, useColorModeValue } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import {
+  fetchPost,
+  fetchOrderedCommentsByPostTime,
+  fetchOrderedCommentsByVoteNumber,
+} from '../utils/firebase/firestore';
+import { isEmptyArray } from 'formik';
+import PostContent from '../components/Post';
+import CreateComment from '../components/CreateComment';
+import Comment from '../components/Comment';
+import DropdownMenu from '../components/DropdownMenu';
+import NoComments from '../components/NoComments';
+import LoadingPost from '../components/LoadingPost';
+import LoadingComment from '../components/LoadingComment';
+
+const Post = () => {
+  const { postId } = useParams();
+  const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
+  const hasNoComments = isEmptyArray(comments);
+  const [isLoadingPost, setIsLoadingPost] = useState(true);
+  const [isLoadingComments, setIsLoadingComments] = useState(true);
+  const dropdownMenuOptions = ['Best', 'Recent', 'Old'];
+  const [sortCommentsMode, setSortCommentsMode] = useState(
+    dropdownMenuOptions[0]
+  );
+
+  const fetchPostData = useCallback(async () => {
+    setIsLoadingPost(true);
+
+    try {
+      const postData = await fetchPost(postId);
+      setPost(postData);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoadingPost(false);
+  }, [postId]);
+
+  const fetchCommentsFunctions = useMemo(
+    () => ({
+      Best: () => fetchOrderedCommentsByVoteNumber(postId),
+      Recent: () => fetchOrderedCommentsByPostTime(postId, 'desc'),
+      Old: () => fetchOrderedCommentsByPostTime(postId, 'asc'),
+    }),
+    [postId]
+  );
+
+  const fetchCommentsData = useCallback(async () => {
+    setIsLoadingComments(true);
+
+    try {
+      const fetchComments = fetchCommentsFunctions[sortCommentsMode];
+      const commentsData = await fetchComments();
+      setComments(commentsData);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoadingComments(false);
+  }, [fetchCommentsFunctions, sortCommentsMode]);
+
+  const changeSortingMode = (event) => {
+    const selectedSortingMode = event.target.textContent;
+    setSortCommentsMode(selectedSortingMode);
+  };
+
+  // Set sort comments mode to recent if not already when a new comment is submitted
+  // If mode is recent already run fetch function instead
+  const handleCommentSubmit = async () => {
+    if (sortCommentsMode === 'Recent') {
+      fetchCommentsData();
+      return;
+    }
+
+    setSortCommentsMode('Recent');
+  };
+
+  // Fetch post when postId changes
+  useEffect(() => {
+    fetchPostData();
+  }, [fetchPostData]);
+
+  // Fetch comments when selected sort mode changes
+  useEffect(() => {
+    fetchCommentsData();
+  }, [fetchCommentsData]);
+
+  return (
+    <Box w='full' py={3} px={{ base: 3, xl: 80 }}>
+      {isLoadingPost ? (
+        <LoadingPost />
+      ) : (
+        <PostContent
+          title={post.title}
+          author={post.author}
+          content={post.content}
+          commentsNumber={post.commentsNumber}
+          votes={post.votes}
+          timestamp={post.timestamp}
+        />
+      )}
+
+      <Stack
+        borderRadius={5}
+        bg={useColorModeValue('brand.light', 'brand.dark')}
+        spacing={4}
+        px={{ base: 2, md: 12 }}
+        py={{ base: 2, md: 6 }}
+        mt={4}
+      >
+        <CreateComment postId={postId} onSubmit={handleCommentSubmit} />
+
+        <Flex direction='column'>
+          <DropdownMenu
+            description={`Sort By: ${sortCommentsMode}`}
+            onClick={changeSortingMode}
+            options={dropdownMenuOptions}
+            selected={sortCommentsMode}
+          />
+          <Divider mt={-1} />
+        </Flex>
+
+        {isLoadingComments &&
+          [...Array(5)].map((v, i) => <LoadingComment key={i} />)}
+
+        {!isLoadingComments &&
+          comments.length &&
+          comments.map((comment) => (
+            <Comment
+              key={comment.id}
+              isPostAuthor={comment.userId === post.userId}
+              author={comment.author}
+              content={comment.content}
+              votes={comment.votes}
+              timestamp={comment.timestamp}
+            />
+          ))}
+
+        {!isLoadingComments && hasNoComments && <NoComments />}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Post;
