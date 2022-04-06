7ce3f424062bfb7b9dd7a5a8c55330a8159ec857
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-04-06 18:17:18 +0100

    Add logic to check if community exists and display appropriate page based on result

--- src/layouts/CommunityLayout.js
+++ src/layouts/CommunityLayout.js
@@ -1,14 +1,56 @@
+import { useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
+import { checkIfCommunityExists } from '../utils/firebase/firestore';
+import { Center } from '@chakra-ui/react';
+import SpinningLogo from '../components/loaders/SpinningLogo';
 import CommunityHeader from '../components/CommunityHeader';
+import Community404 from '../pages/Community404';
 
 const CommunityLayout = () => {
   const { communityName } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isCommunity, setIsCommunity] = useState(null);
 
-  return (
+  useEffect(() => {
+    const verifyIfCommunityExists = async () => {
+      setIsLoading(true);
+
+      try {
+        const communityExists = await checkIfCommunityExists(communityName);
+
+        setIsCommunity(communityExists);
+      } catch (error) {
+        console.log(error);
+      }
+
+      setIsLoading(false);
+    };
+
+    verifyIfCommunityExists();
+  }, [communityName]);
+
+  if (isLoading) {
+    return (
+      <Center
+        position='absolute'
+        top={0}
+        left={0}
+        w='100vw'
+        h='100vh'
+        zIndex={-1}
+      >
+        <SpinningLogo />
+      </Center>
+    );
+  }
+
+  return isCommunity ? (
     <>
       <CommunityHeader communityName={communityName} />
       <Outlet />
     </>
+  ) : (
+    <Community404 />
   );
 };
 
