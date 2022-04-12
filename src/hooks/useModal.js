b84bf5d57d4f584e0b30fc808ca21f5dd1d6a8ff
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-04-12 10:22:32 +0100

    Handle more logic inside useModal hook

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,16 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 import * as ui from '../store/ui';
 
+/* Used in case of a null value passed to useSelector to prevent error since
+   useSelector must receive a function.
+*/
+const placeholderFunction = () => {};
+
 const useModal = (name) => {
   const dispatch = useDispatch();
 
-  const isOpen = useSelector(ui[`select${name}Status`]);
+  const isOpen = useSelector(ui[`select${name}Status`] || placeholderFunction);
+
+  const isLogin = useSelector(ui[`select${name}Mode`] || placeholderFunction);
 
   const onOpen = () => dispatch(ui[`open${name}`]());
 
   const onClose = () => dispatch(ui[`close${name}`]());
 
-  return { isOpen, onOpen, onClose };
+  const changeMode = () => dispatch(ui[`change${name}Mode`]());
+
+  return { isOpen, isLogin, onOpen, onClose, changeMode };
 };
 
 export default useModal;
