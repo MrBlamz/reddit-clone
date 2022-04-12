8a26669e2835ad60b9646e05982f2094544cef30
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-04-12 19:32:44 +0100

    Create NoPosts component

---
+++ src/components/NoPosts.js
@@ -0,0 +1,29 @@
+import { Button, Center, Flex, Heading, VStack } from '@chakra-ui/react';
+
+const NoPosts = () => {
+  return (
+    <Center
+      position='absolute'
+      top={0}
+      left={0}
+      direction='column'
+      w='100vw'
+      h='100vh'
+      zIndex={-1}
+    >
+      <Flex direction='column' alignItems='center'>
+        <VStack spacing={3}>
+          <Heading fontSize={18}>There are no posts in this subreddit</Heading>
+          <Heading fontSize={14}>
+            Be the first to till this fertile land.
+          </Heading>
+        </VStack>
+        <Button variant='primary' w={200} mt={4}>
+          Add a post
+        </Button>
+      </Flex>
+    </Center>
+  );
+};
+
+export default NoPosts;
