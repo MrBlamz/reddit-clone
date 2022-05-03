57841f0288e2abdfd4ad1970a660585d0cd640a9
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-05-03 09:21:42 +0100

    Add description field to NewCommunityForm

--- src/components/CreateCommunityModal.js
+++ src/components/CreateCommunityModal.js
@@ -3,19 +3,15 @@ import {
   Box,
   Divider,
   Heading,
-  HStack,
   Modal,
   ModalCloseButton,
   ModalContent,
   ModalOverlay,
-  Text,
   useColorModeValue,
   VStack,
 } from '@chakra-ui/react';
 import useModal from '../hooks/useModal';
-import { IoInformationCircleOutline } from 'react-icons/io5';
 import NewCommunityForm from './NewCommunityForm';
-import TouchFriendlyTooltip from './TouchFriendlyTooltip';
 
 const CreateCommunityModal = () => {
   const ref = useRef();
@@ -24,7 +20,6 @@ const CreateCommunityModal = () => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered initialFocusRef={ref}>
       <ModalOverlay backdropFilter='auto' backdropBlur='3px' />
-
       <ModalContent
         maxW={{ base: 'full', sm: '90%', md: 600 }}
         bg={useColorModeValue('brand.light', 'brand.dark')}
@@ -37,34 +32,7 @@ const CreateCommunityModal = () => {
           <Box as='header' w='full'>
             <Heading fontSize={16}>Create a community</Heading>
           </Box>
-
           <Divider />
-
-          <VStack w='full' alignItems='flex-start' spacing={1}>
-            <Heading fontSize={16}>Name</Heading>
-            <HStack
-              fontSize={12}
-              color={useColorModeValue('brand.iconLight', 'brand.iconDark')}
-            >
-              <Text>
-                Community names including capitalization cannot be changed.
-              </Text>
-              <TouchFriendlyTooltip
-                label={
-                  'Names cannot have spaces (e.g., "r/bookclub" not "r/book club"), must be between 3-21 characters, and underscores ("_") are the only special characters allowed. Avoid using solely trademarked names (e.g., "r/FansOfAcme" not "r/Acme").'
-                }
-                fontSize={12}
-                fontWeight='bold'
-                bg='#000'
-                color='#fff'
-                w={200}
-                textAlign='center'
-              >
-                <IoInformationCircleOutline size={16} />
-              </TouchFriendlyTooltip>
-            </HStack>
-          </VStack>
-
           <Box w='full'>
             <NewCommunityForm innerRef={ref} onClose={onClose} />
           </Box>
--- src/components/NewCommunityForm.js
+++ src/components/NewCommunityForm.js
@@ -1,9 +1,9 @@
 import {
-  Box,
   Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
+  Heading,
   HStack,
   Text,
   useColorModeValue,
@@ -11,30 +11,45 @@ import {
 } from '@chakra-ui/react';
 import { Field, Form, Formik } from 'formik';
 import { useState } from 'react';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 import {
   checkIfCommunityExists,
   createCommunity,
 } from '../utils/firebase/firestore';
+import AutoResizeTextArea from './AutoResizeTextArea';
 import InputWithPermanentPlaceholder from './forms/InputWithPermanentPlaceholder';
+import TouchFriendlyTooltip from './TouchFriendlyTooltip';
 
 const COMMUNITY_NAME_MAX_CHARACTERS = 21;
 const ERROR_MESSAGE =
   'Community names must be between 3-21 characters, and can only contain letters, numbers, or underscores.';
+const DESCRIPTION_ERROR_MESSAGE =
+  'Community description must be between 5-100 characters, and can only contain letters, numbers and spaces';
+
+const initialValues = { name: '', description: '' };
 
 const validationSchema = yup.object({
-  communityName: yup
+  name: yup
     .string()
     .required('A community name is required')
     .min(3, ERROR_MESSAGE)
     .max(COMMUNITY_NAME_MAX_CHARACTERS, ERROR_MESSAGE)
     .matches(/^[_a-zA-Z0-9]+$/, ERROR_MESSAGE),
+  description: yup
+    .string()
+    .required('A community description is required')
+    .min(5, DESCRIPTION_ERROR_MESSAGE)
+    .max(100, DESCRIPTION_ERROR_MESSAGE)
+    .matches(/^[a-zA-Z0-9]+(\s+[a-zA-Z0-9]+)*$/, DESCRIPTION_ERROR_MESSAGE),
 });
 
 const NewCommunityForm = ({ innerRef, onClose }) => {
   const navigate = useNavigate();
   const infoColor = useColorModeValue('brand.iconLight', 'brand.iconDark');
+  const color = useColorModeValue('brand.iconLight', 'brand.iconDark');
+  const errorBorderColor = useColorModeValue('crimson', 'red.300');
   const [charactersLeft, setCharactersLeft] = useState(
     COMMUNITY_NAME_MAX_CHARACTERS
   );
@@ -49,23 +64,23 @@ const NewCommunityForm = ({ innerRef, onClose }) => {
   };
 
   const handleFormSubmit = async (values, actions) => {
-    const { communityName } = values;
+    const { name, description } = values;
 
     try {
-      const communityData = await checkIfCommunityExists(communityName);
+      const communityData = await checkIfCommunityExists(name);
 
       if (communityData) {
         actions.setFieldError(
-          'communityName',
-          `Sorry, r/${communityName} is taken. Try another.`
+          'name',
+          `Sorry, r/${name} is taken. Try another.`
         );
 
         return;
       }
 
-      await createCommunity(communityName);
+      await createCommunity(name, description);
 
-      navigate(`/r/${communityName}`);
+      navigate(`/r/${name}`);
       onClose();
     } catch (error) {
       console.log(error);
@@ -74,31 +89,51 @@ const NewCommunityForm = ({ innerRef, onClose }) => {
 
   return (
     <Formik
-      initialValues={{ communityName: '' }}
+      initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={handleFormSubmit}
       validateOnChange={false}
     >
       {(props) => (
         <Form>
-          <Box minH={{ base: 150, md: 140 }} position='relative'>
-            <Field name='communityName'>
-              {({ field, form }) => (
-                <FormControl
-                  isInvalid={
-                    form.errors.communityName && form.touched.communityName
+          <VStack>
+            <VStack w='full' alignItems='flex-start' spacing={1}>
+              <Heading fontSize={16}>Name</Heading>
+              <HStack fontSize={12} color={color}>
+                <Text>
+                  Community names including capitalization cannot be changed.
+                </Text>
+                <TouchFriendlyTooltip
+                  label={
+                    'Names cannot have spaces (e.g., "r/bookclub" not "r/book club"), must be between 3-21 characters, and underscores ("_") are the only special characters allowed. Avoid using solely trademarked names (e.g., "r/FansOfAcme" not "r/Acme").'
                   }
+                  fontSize={12}
+                  fontWeight='bold'
+                  bg='#000'
+                  color='#fff'
+                  w={200}
+                  textAlign='center'
                 >
-                  <FormLabel htmlFor='communityName' hidden>
+                  <IoInformationCircleOutline size={16} />
+                </TouchFriendlyTooltip>
+              </HStack>
+            </VStack>
+
+            <Field name='name'>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.name && form.touched.name}>
+                  <FormLabel htmlFor='name' hidden>
                     Community Name
                   </FormLabel>
                   <InputWithPermanentPlaceholder
                     maxLength={21}
                     placeholder='r/'
                     innerRef={innerRef}
-                    id='communityName'
+                    id='name'
                     {...field}
                     onChange={handleInputChange(field, form)}
+                    border='2px solid'
+                    borderColor={form.errors.name && errorBorderColor}
                   />
 
                   <VStack spacing={1} w='full' position='relative'>
@@ -113,20 +148,48 @@ const NewCommunityForm = ({ innerRef, onClose }) => {
                       fontWeight='bold'
                       fontSize={12}
                     >
-                      {form.errors.communityName}
+                      {form.errors.name}
                     </FormErrorMessage>
                   </VStack>
                 </FormControl>
               )}
             </Field>
 
-            <HStack
-              position='absolute'
-              bottom={0}
-              right={0}
-              justifyContent='center'
-              spacing={3}
-            >
+            <VStack w='full' alignItems='flex-start' spacing={1}>
+              <Heading fontSize={16}>Description</Heading>
+              <HStack fontSize={12} color={color}>
+                <Text>Summary about this community.</Text>
+              </HStack>
+            </VStack>
+
+            <Field name='description'>
+              {({ field, form }) => (
+                <FormControl
+                  isInvalid={
+                    form.errors.description && form.touched.description
+                  }
+                >
+                  <FormLabel htmlFor='communityDescription' hidden>
+                    Community Description
+                  </FormLabel>
+
+                  <AutoResizeTextArea
+                    minH={32}
+                    whiteSpace='pre-wrap'
+                    maxLength={100}
+                    id='communityDescription'
+                    {...field}
+                    borderColor={form.errors.description && errorBorderColor}
+                  />
+
+                  <FormErrorMessage w='inherit' fontWeight='bold' fontSize={12}>
+                    {form.errors.description}
+                  </FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+
+            <HStack w='full' justifyContent='flex-end' spacing={3}>
               <Button variant='secondary' fontSize={14} onClick={onClose}>
                 Cancel
               </Button>
@@ -139,7 +202,7 @@ const NewCommunityForm = ({ innerRef, onClose }) => {
                 Create Community
               </Button>
             </HStack>
-          </Box>
+          </VStack>
         </Form>
       )}
     </Formik>
