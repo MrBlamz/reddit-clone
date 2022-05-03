61a8e6eabd29fb2e0dcfc8e238e73ac797c513a2
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-05-03 09:47:01 +0100

    Create AboutCommunity component

---
+++ src/components/AboutCommunity.js
@@ -0,0 +1,49 @@
+import {
+  Button,
+  Divider,
+  Flex,
+  Icon,
+  Text,
+  useColorModeValue,
+  VStack,
+} from '@chakra-ui/react';
+import { format } from 'date-fns';
+import { GiCakeSlice } from 'react-icons/gi';
+import { useNavigate } from 'react-router-dom';
+
+export const AboutCommunity = ({ description, timestamp, ...props }) => {
+  const navigate = useNavigate();
+  const creationDateAsString = format(timestamp, 'MMM d,yyyy');
+
+  const handleClick = () => navigate('submit');
+
+  return (
+    <Flex
+      bg={useColorModeValue('brand.light', 'brand.dark')}
+      p={3}
+      border='1px'
+      borderColor={useColorModeValue('brand.borderLight', 'brand.borderDark')}
+      borderRadius={5}
+      {...props}
+    >
+      <VStack w='full' alignItems='flex-start' spacing={4} fontSize={14}>
+        <Text
+          fontSize='sm'
+          color={useColorModeValue('brand.iconLight', 'brand.iconDark')}
+          fontWeight='bold'
+        >
+          About Community
+        </Text>
+        <Text>{description}</Text>
+        <Divider />
+        <Flex alignItems='center'>
+          <Icon as={GiCakeSlice} transform='rotateY(180deg)' boxSize={6} />
+          <Text ml={2}>Created {creationDateAsString}</Text>
+        </Flex>
+        <Button w='full' variant='primary' onClick={handleClick}>
+          Create Post
+        </Button>
+      </VStack>
+    </Flex>
+  );
+};
