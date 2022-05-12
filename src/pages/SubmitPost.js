bd0096694f7bfb7238357fa15bc2509755dc00ee
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-05-12 11:29:33 +0100

    Change SubmitPost padding

--- src/pages/SubmitPost.js
+++ src/pages/SubmitPost.js
@@ -2,32 +2,31 @@ import {
   Box,
   Divider,
   Fade,
-  Flex,
   Heading,
   useColorModeValue,
+  VStack,
 } from '@chakra-ui/react';
 import NewPostForm from '../components/NewPostForm';
 
 const SubmitPost = () => {
   return (
     <Fade in={true}>
-      <Box px={{ base: 3, md: '5rem', xl: '28rem' }} py={3}>
-        <Flex
+      <Box px={{ base: 3, md: '5rem', xl: '28rem' }} py={{ base: 3, xl: 10 }}>
+        <VStack
+          spacing={3}
           p={3}
-          direction='column'
-          borderRadius={5}
           bg={useColorModeValue('brand.light', 'brand.dark')}
-          alignItems='flex-start'
+          borderRadius={5}
         >
           <Box as='header' w='full'>
             <Heading fontSize={18}>Create a post</Heading>
             <Divider mt={3} />
           </Box>
 
-          <Box w='full' mt={4}>
+          <Box w='full'>
             <NewPostForm />
           </Box>
-        </Flex>
+        </VStack>
       </Box>
     </Fade>
   );
