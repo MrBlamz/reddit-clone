6dd817cfffb87e6a0c05d7ad1bae31f9f3e139fb
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-05-13 15:25:42 +0100

    Remove unused ActionButton variants

--- src/components/buttons/ActionButton.js
+++ src/components/buttons/ActionButton.js
@@ -1,7 +1,4 @@
 import { Button, Text } from '@chakra-ui/react';
-import { BiShare } from 'react-icons/bi';
-import { BsBookmark } from 'react-icons/bs';
-import { VscComment } from 'react-icons/vsc';
 
 export const ActionButton = ({ ariaLabel, icon, text, ...rest }) => (
   <Button
@@ -13,33 +10,6 @@ export const ActionButton = ({ ariaLabel, icon, text, ...rest }) => (
   >
     <Text fontSize='xs'>{text}</Text>
   </Button>
-);
-
-export const CommentsButton = ({ commentsNumber, ...rest }) => (
-  <ActionButton
-    ariaLabel='Open Comments'
-    icon={<VscComment />}
-    text={`${commentsNumber} ${commentsNumber === 1 ? 'Comment' : 'Comments'}`}
-    {...rest}
-  />
-);
-
-export const ShareButton = ({ ...rest }) => (
-  <ActionButton
-    ariaLabel='Share post'
-    icon={<BiShare style={{ transform: 'rotateY(180deg)' }} />}
-    text='Share'
-    {...rest}
-  />
-);
-
-export const SaveButton = ({ ...rest }) => (
-  <ActionButton
-    ariaLabel='Save post'
-    icon={<BsBookmark />}
-    text='Save'
-    {...rest}
-  />
 );
 
 export default ActionButton;
